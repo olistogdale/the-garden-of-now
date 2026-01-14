a70be84be6c4407b08a41f883b0c1fa29170b2df
Oli Stogdale <oli.stogdale@gmail.com>
2026-01-14 18:40:30 +0000

    fix: created favourites controller index to streamline architecture

---
+++ server/src/controllers/favourites/index.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+export {getFavouriteRecipes} from './get-favourite-recipes'
+export {postFavouriteRecipe} from './post-favourite-recipe'
+export {deleteFavouriteRecipe} from './delete-favourite-recipe'
