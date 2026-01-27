ce624de3e18d41d5a2536af2c32d8ba478e0beb6
Oli Stogdale <oli.stogdale@gmail.com>
2026-01-27 12:15:08 +0000

    fix: included missing middleware in auth and logout routes

--- server/src/routers/auth-router.ts
+++ server/src/routers/auth-router.ts
@@ -3,12 +3,13 @@
 import Router from 'koa-router';
 
 import { registerUser, loginUser, authUser, logoutUser } from '../controllers/auth/index';
+import { requireAuth } from '../middleware/auth';
 
 const authRouter = new Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
-authRouter.get('/auth/me', authUser); 
-authRouter.post('/logout', logoutUser);
+authRouter.get('/auth/me', requireAuth, authUser); 
+authRouter.post('/logout', requireAuth, logoutUser);
 
 export default authRouter;
