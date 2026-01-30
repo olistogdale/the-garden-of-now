1de423cfe3806a51701441ab3966d0deda784b75
Oli Stogdale <oli.stogdale@gmail.com>
2026-01-30 23:49:24 +0000

    chore: deleted redundant controllers/type files

--- server/src/controllers/favourites/delete-favourite-recipe.ts
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export const deleteFavouriteRecipe = async function() {
-  
-}
--- server/src/types/recipe-types.ts
+++
@@ -1,6 +0,0 @@
-import type { RecipeCardT } from '../../../data/recipes/types/recipe-types'
-
-export type RecipesFacetResultT = {
-  results: RecipeCardT[];
-  totalCount: { count: number }[];
-};
