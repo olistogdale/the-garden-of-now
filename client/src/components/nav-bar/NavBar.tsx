005dcf98558e8fcb67500d3e85a4dd3fae8f68f1
Oli Stogdale <oli.stogdale@gmail.com>
2026-02-12 13:05:09 +0000

    fix: adjusted spacing to align nav-bar components with main page features.

--- client/src/components/nav-bar/NavBar.tsx
+++ client/src/components/nav-bar/NavBar.tsx
@@ -11,13 +11,13 @@ export function NavBar() {
   const { auth } = useAuth();
   const { pathname } = useLocation();
 
-  const navLinkClassname: NavLinkProps['className'] = ({ isActive }) => (isActive ? 'navbar__navlink active' : 'navbar__navlink')
+  const navLinkClassname: NavLinkProps['className'] = ({ isActive }) => (isActive ? 'navlink active' : 'navlink')
 
   return (
     <div className="navbar">        
-      <nav className="navbar__links-left" aria-label="Primary">
+      <nav className="navbar__navlinks--left" aria-label="Primary">
         {pathname === '/' ? (
-          <div className="navbar__back-button-placeholder"/>
+          <div className="back-button-placeholder"/>
         ) : (
           <BackButton />
         )}
@@ -26,25 +26,24 @@ export function NavBar() {
       <NavLink to="/" className="navbar__brand" aria-label="Home">GARDEN <br className="break"/>OF NOW</NavLink>
 
       {auth ? (
-        <nav className="navbar__links-right" aria-label="User">
+        <nav className="navbar__navlinks--right" aria-label="User">
           <NavLink to="/favourites" className={navLinkClassname} >
-            <Star className="navbar__navlink-icon"/>
-            <span className="navbar__navlink-text">FAVOURITES</span>
+            <Star className="navlink-icon"/>
+            <span className="navlink-text">FAVOURITES</span>
           </NavLink>
           <NavLink to="/profile" className={navLinkClassname} >
-            <User className="navbar__navlink-icon"/>
-            <span className="navbar__navlink-text">PROFILE</span>
+            <User className="navlink-icon"/>
+            <span className="navlink-text">PROFILE</span>
           </NavLink>
         </nav>
       ) : (
-        <nav className="navbar__links-right" aria-label="User">
+        <nav className="navbar__navlinks--right" aria-label="User">
           <NavLink to="/login" className={navLinkClassname} >
-            <KeyRound className="navbar__navlink-icon"/>
-            <span className="navbar__navlink-text">LOGIN/REGISTER</span>
+            <KeyRound className="navlink-icon"/>
+            <span className="navlink-text">LOGIN/REGISTER</span>
           </NavLink>
         </nav>
       )}
     </div>
   )
-
 }
