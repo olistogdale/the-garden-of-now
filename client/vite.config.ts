961ef03c32ded706f6adaaeb1d286dc82ec99d62
Oli Stogdale <oli.stogdale@gmail.com>
2026-02-25 13:04:14 +0000

    fix: fixed vite version mismatch and reinstalled all packages.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+    css: true
+  },
   server: {
     host: true,
     proxy: {
