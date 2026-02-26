cc5fc92a6486ffbaf41416262e79c7514d1ecf0e
Oli Stogdale <oli.stogdale@gmail.com>
2026-02-26 16:07:42 +0000

    fix: updated isInSeason to feature input guard in response to test failure.

--- server/src/utilities/seasonality-utils.ts
+++ server/src/utilities/seasonality-utils.ts
@@ -1,6 +1,8 @@
 import type { RecipeCardT } from "../../../data/recipes/types/recipe-types"
 
 export function isInSeason(recipe: RecipeCardT, ingredientsSet: Set<string>) {
+  if (recipe.groupedIngredients.length === 0) return false;
+
   return recipe.groupedIngredients.every((ingredientOptions) => {
     return ingredientOptions.some((ingredient) => ingredientsSet.has(ingredient))
   })
