92c1668cdc1327114dfb11a437f9328b03db318f
Oli Stogdale <oli.stogdale@gmail.com>
2026-02-27 14:46:28 +0000

    fix: added input guards on relevant jwt utility functions.

--- server/src/utilities/jwt-utils.ts
+++ server/src/utilities/jwt-utils.ts
@@ -5,6 +5,8 @@ import type { JwtPayload, SignOptions } from 'jsonwebtoken';
 import { config } from '../config'
   
 export const signAccessToken = function(userID: string) {
+  if (userID.trim().length === 0) throw new Error("Invalid user ID.")
+  
   return jwt.sign(
     { sub: userID },
     config.jwtSecret,
@@ -13,5 +15,7 @@ export const signAccessToken = function(userID: string) {
 }
 
 export const verifyAccessToken = function(token: string) {
+  if (token.trim().length === 0) throw Error("Invalid token.")
+  
   return jwt.verify(token, config.jwtSecret) as JwtPayload;
 }
