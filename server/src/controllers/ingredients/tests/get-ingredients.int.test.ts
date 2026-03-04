781c30d74c7051fded1581f7b45b3b7aaeb71820
Oli Stogdale <oli.stogdale@gmail.com>
2026-03-04 12:29:57 +0000

    feat: implemented integration test suits for all ingredient and recipe endpoints, happy and sad paths.

---
+++ server/src/controllers/ingredients/tests/get-ingredients.int.test.ts
@@ -0,0 +1,130 @@
+import { describe, it, expect, beforeAll, afterAll, beforeEach } from 'vitest';
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import { createApp } from '../../../app';
+import { connectDB, disconnectDB } from '../../../database/db';
+import { seasonalIngredientModel } from '../../../models/seasonal-ingredient-model';
+import { nonSeasonalIngredientModel } from '../../../models/non-seasonal-ingredient-model';
+
+let mongo: MongoMemoryServer;
+
+describe("GET ingredients/:month", () => {
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create({
+      binary: { version: "6.0.15" }
+    });
+    await connectDB(mongo.getUri());
+  });
+
+  afterAll(async () => {
+    await disconnectDB();
+    await mongo.stop();
+  });
+
+  beforeEach(async () => {
+    await seasonalIngredientModel.deleteMany({});
+    await nonSeasonalIngredientModel.deleteMany({});
+  });
+
+  async function expectError(app: any, query: string, errorCode: number, error: string) {
+    const res = await request(app).get(`/ingredients/${query}`);
+    expect(res.status).toBe(errorCode);
+    expect(res.body.error).toBe(error);
+  }
+ 
+  it("returns deduped, normalized available ingredient names for a valid month (200)", async () => {
+    await seasonalIngredientModel.create([
+      {
+        "name": "orange",
+        "seasonality": [
+          "july",
+          "august",
+          "september"
+        ],
+        "foodGroup": "fruit",
+        "altNames": [
+          "mandarin orange",
+          "seville orange"
+        ]
+      },
+      {
+        "name": "lemon",
+        "seasonality": [
+          "january",
+          "february",
+          "march"
+        ],
+        "foodGroup": "fruit",
+      },
+      {
+        "name": "tomato",
+        "seasonality": [
+          "january",
+          "february",
+          "march",
+          "april",
+          "may",
+          "june",
+          "july",
+          "august",
+          "september",
+          "october",
+          "november",
+          "december"
+        ],
+        "foodGroup": "vegetables"
+      },
+      {
+        "name": "grapefruit",
+        "seasonality": [
+          "april",
+          "may",
+          "june"
+        ],
+        "foodGroup": "fruit"
+      },
+      {
+        "name": "lime",
+        "seasonality": [
+          "october",
+          "november",
+          "december"
+        ],
+        "foodGroup": "fruit"
+      }
+    ]);
+
+    await nonSeasonalIngredientModel.create([
+      {
+        "name": "tomato",
+        "foodGroup": "juice"
+      },
+      {
+        "name": "apple chutney",
+        "foodGroup": "condiments",
+        "altNames": [
+          "apple and pear chutney",
+          "apple & pear chutney"
+        ]
+      }
+    ]);
+
+    const app = createApp().callback();
+
+    const res = await request(app).get("/ingredients/february");
+
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body.ingredients)).toBe(true);
+    expect(new Set(res.body.ingredients)).toEqual(new Set(['lemon', 'tomato', 'apple chutney', "apple & pear chutney", "apple and pear chutney"]));
+  });
+
+  it("throws an error for invalid input (400)", async () => {
+    const app = createApp().callback();
+    
+    const errorReturned = "Invalid input. Please specify a correct month."
+      
+    await expectError(app, "%20%20%20", 400, errorReturned);
+    await expectError(app, 'easter', 400, errorReturned);
+  });
+});
---
+++ server/src/controllers/recipes/tests/get-recipe.int.test.ts
@@ -0,0 +1,94 @@
+import { describe, it, expect, beforeAll, afterAll, beforeEach } from "vitest";
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+import { createApp } from "../../../app";
+import { connectDB, disconnectDB } from "../../../database/db";
+import { recipeModel } from "../../../models/recipe-model";
+import { makeRecipe } from "./test-utils";
+
+let mongo: MongoMemoryServer;
+
+describe("GET /recipes/:id", () => {
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create({
+      binary: { version: "6.0.15" }
+    })
+    await connectDB(mongo.getUri());
+  })
+
+  afterAll(async () => {
+    await disconnectDB();
+    await mongo.stop();
+  })
+
+  beforeEach(async () => {
+    await recipeModel.deleteMany({})
+  })
+
+  async function expectError(app: any, query: any, errorCode: number, error: string) {
+    const res = await request(app).get(`/recipes/${query}`);
+    expect(res.status).toBe(errorCode);
+    error && expect(res.body).toMatchObject({ error });
+  }
+
+  it("returns a full recipe entry for a given specified ID", async () => {
+    let recipeArray = [];
+    let i = 0
+
+    while (i < 30) {
+      recipeArray.push(makeRecipe({ _id: "id" + i, name: "name" + i, groupedIngredients: [["ingredient"]]}));
+      i++;
+    }
+
+    const returnedRecipe = {
+      _id: "gnocchi_squash_2",
+      name: "Gnocchi with roasted squash & goat’s cheese",
+      image: [
+        {
+          url: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-9163_12-6d73646.jpg?resize=440,400",
+          width: 440,
+          height: 400
+        }
+      ],
+      cookTime: "PT20M",
+      totalTime: "PT35M",
+      skillLevel: "Easy",
+      groupedIngredients: [
+        ["butternut squash"],
+        ["garlic clove"],
+        ["olive oil"],
+        ["gnocchi"],
+        ["spinach"],
+        ["goat's cheese"]
+      ]
+    }
+
+    recipeArray.push(makeRecipe(returnedRecipe));
+
+    await recipeModel.create(recipeArray);
+
+    const app = createApp().callback();
+
+    const res = await request(app).get("/recipes/gnocchi_squash_2");
+
+    expect(res.status).toBe(200);
+    expect(res.body.recipe).toMatchObject(returnedRecipe);
+  })
+
+  it("throws an error for invalid input (400)", async () => {
+    const app = createApp().callback();
+
+    const errorReturned = "Invalid recipe ID. Please provide a valid recipe ID."
+
+    await expectError(app, "%20%20%20", 400, errorReturned);
+  })
+
+  it("throws an error for invalid recipe ID (404)", async () => {
+    const app = createApp().callback();
+    
+    const errorReturned = "Recipe irish_pork_1 not found."
+
+    await expectError(app, "irish_pork_1", 404, errorReturned);
+  })
+})
