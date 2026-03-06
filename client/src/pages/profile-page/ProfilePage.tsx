31bfe8437040630253d429d42ac25841a0dcbf7a
Oli Stogdale <oli.stogdale@gmail.com>
2026-03-06 18:51:27 +0000

    feat: updated content and structure of user profile page, including detail update functionality.

--- client/src/pages/profile-page/ProfilePage.tsx
+++ client/src/pages/profile-page/ProfilePage.tsx
@@ -1,19 +1,24 @@
+import "./ProfilePage.css"
+
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+
 import { useAuth } from '../../hooks/useAuth';
 import { StatusPanel } from '../../components/status-panel/StatusPanel';
+import { useStableLoading } from '../../hooks/useStableLoading';
+import { Modal } from "../../components/modal/Modal";
 
 import type { StatusT } from '../../types/status-types';
-import { useStableLoading } from '../../hooks/useStableLoading';
 
 export function ProfilePage () { 
   const navigate = useNavigate();
   const { logout, auth } = useAuth();
 
   const [logoutStatus, setLogoutStatus] = useState <StatusT> ('idle');
-  const [logoutError, setLogoutError] = useState <string | null> (null) 
+  const [logoutError, setLogoutError] = useState <string | null> (null);
+  const [profileModal, setProfileModal] = useState <boolean> (false);
 
-  const onLogout = async function () {
+  const handleLogout = async function () {
     try {
       setLogoutStatus('loading');
       setLogoutError(null);
@@ -26,36 +31,57 @@ export function ProfilePage () {
     }
   }
 
+  const handleModal = async function () {
+    setProfileModal((prev) => !prev)    
+  }
+
   const showLoading = useStableLoading(logoutStatus === "loading");
   
-  if (logoutStatus === "error") return <StatusPanel mode = "error" message={`Could not log out ${logoutError ? `: ${logoutError}` : '.'}`} />;
+  if (logoutStatus === "error") return <StatusPanel mode="error" message={`Could not log out ${logoutError ? `: ${logoutError}` : '.'}`} />;
 
   if (showLoading) return <StatusPanel mode="loading" message="Logging out of your account..." />;
 
   return (
-    <div className="profile-page">
-      <section className="profile-page__header">
-        <h1 className="profile-page__title">Profile</h1>
-        {auth ? (
-          <p className="profile-page__subtitle">
-            Signed in as <strong>{auth.email}</strong>
+    <div className="profile">
+      {profileModal && <Modal handleModal={handleModal}/>}
+
+      <section className="profile__user">
+        <div className="user__header-blank"/>
+        <div className="user__dashboard">
+          {auth ? (
+            <div className="user__id">
+              <p className="user__name">
+                User: <strong>{auth.firstName} {auth.lastName}</strong>
+              </p>
+              <p className="user__subtitle">
+                Signed in as <strong>{auth.email}</strong>
+              </p>
+            </div>
+          ) : null}
+
+          <div className="user__controls">
+             <button
+                className="user__logout"
+                type="button"
+                onClick={handleLogout}
+                disabled={logoutStatus === "loading"}
+              >
+                LOG OUT
+              </button>
+
+              <button
+                className="user__change-password"
+                type="button"
+                onClick={handleModal}
+                disabled={logoutStatus === "loading"}
+              >
+                CHANGE PASSWORD
+              </button>
+          </div>
+          <p className="user__hint">
+            Looking for dinner-time inspiration? <br /><Link to="/recipes">Browse recipes</Link>
           </p>
-        ) : null}
-      </section>
-
-      <section className="profile-page__actions">
-        <button
-          className="profile-page__button"
-          type="button"
-          onClick={onLogout}
-          disabled={logoutStatus === "loading"}
-        >
-          Log out
-        </button>
-
-        <p className="profile-page__hint">
-          Want recipes? <Link to="/recipes">Browse recipes</Link>
-        </p>
+        </div>
       </section>
     </div>
   );
