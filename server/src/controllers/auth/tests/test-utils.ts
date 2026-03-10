6b5a9c9a92071313bfa059986d3754357829b414
Oli Stogdale <oli.stogdale@gmail.com>
2026-03-10 17:32:47 +0000

    chore: streamlined test logic by moving common testing functions to utils folder.

--- server/src/controllers/auth/tests/test-utils.ts
+++
@@ -1,9 +0,0 @@
-export function extractAccessToken(res: any) {
-  const rawSetCookie = res.headers["set-cookie"] ?? [];
-  const setCookie = Array.isArray(rawSetCookie)
-    ? rawSetCookie
-    : rawSetCookie
-      ? [rawSetCookie]
-      : [];
-  return setCookie.filter((cookie) => cookie.startsWith("accessToken="))[0];
-}
